ef58bab2b75f6c907f00500ea72e88e0069b8756
DenisFrolkov <deniska-frolkov@mail.ru>
2022-02-19 20:04:33 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print("Введите имя: ")
+    val name = readLine()
+    print("Введите фамилию: ")
+    val surname = readLine()
+    print("Введите возраст: ")
+    val age :Int = readLine()!!.toInt()
+    if (age > 18) println("$name $surname ему есть 18, пропускайте.")
+    if (age < 14) println("$name $surname младше 14 лет, вызовите полицию.")
+    if (age > 14 && age < 18) println ("$name $surname не пускать в торговый центр.")
+}
