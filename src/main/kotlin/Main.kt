baee77ea7649d5d6b7f958d598ff1321539e4fd8
DenisFrolkov <deniska-frolkov@mail.ru>
2022-02-20 20:17:05 +0300

    HW_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main() {
+
     val tc: Array<Any> = arrayOf(1, 2, 3, 4, 5)
     print("Введите имя: ")
     val name = readLine()
@@ -7,7 +8,6 @@ fun main() {
     print("Введите возраст: ")
     val age :Int = readLine()!!.toInt()
     tc[0] = "$name $surname $age"
-    println(tc[0])
 
     print("Введите имя: ")
     val name_2 = readLine()
@@ -16,7 +16,6 @@ fun main() {
     print("Введите возраст: ")
     val age_2 :Int = readLine()!!.toInt()
     tc[1] = "$name_2 $surname_2 $age_2"
-    println(tc[1])
 
     print("Введите имя: ")
     val name_3 = readLine()
@@ -25,7 +24,6 @@ fun main() {
     print("Введите возраст: ")
     val age_3 :Int = readLine()!!.toInt()
     tc[2] = "$name_3 $surname_3 $age_3"
-    println(tc[2])
 
     print("Введите имя: ")
     val name_4 = readLine()
@@ -34,7 +32,6 @@ fun main() {
     print("Введите возраст: ")
     val age_4 :Int = readLine()!!.toInt()
     tc[3] = "$name_4 $surname_4 $age_4"
-    println(tc[3])
 
     print("Введите имя: ")
     val name_5 = readLine()
@@ -43,9 +40,7 @@ fun main() {
     print("Введите возраст: ")
     val age_5 :Int = readLine()!!.toInt()
     tc[4] = "$name_5 $surname_5 $age_5"
-    println(tc[4])
     println("Все посетители: ")
-    tc.forEach{el ->
+    for(el in tc)
         println(el)
-    }
 }
